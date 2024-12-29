5949f3179dee61ea4caa9de7be15d00b5cfd613e
Swapnil <pawarswapnil3305@gmail.com>
2024-12-29 18:17:12 +0530

    added dynamic port

--- app.js
+++ app.js
@@ -1,50 +1,51 @@
-if(process.env.NODE_ENV != "production") {
-    require('dotenv').config();
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
 }
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const session = require("express-session");
-const flash = require('connect-flash');
+const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
-const methodOverride = require('method-override');
-const ejsMate = require('ejs-mate');
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 const mongoose = require("mongoose");
 
-const listingRouter = require('./routes/listing.js');
-const userRouter = require('./routes/user.js');
+const listingRouter = require("./routes/listing.js");
+const userRouter = require("./routes/user.js");
 
 const MONGO_URL = "mongodb://localhost:27017/gostream";
 
 main()
-.then(() => {
+  .then(() => {
     console.log("connected to db");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-})
-async function main(){
-   await mongoose.connect(MONGO_URL);
+  });
+async function main() {
+  await mongoose.connect(MONGO_URL);
 }
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 const sessionOptions = {
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        expires: Date.now()+ 7 * 24 * 60 * 60 * 1000,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true
-    }
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
 };
 
 app.use(session(sessionOptions));
@@ -58,21 +59,21 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
-    next();
-})
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  next();
+});
 
 app.use("/listings", listingRouter);
 app.use("/", userRouter);
 
-
-app.use((err, req, res, next)=>{
-    let {statusCode=500, message="Something went wrong!"} = err;
-    res.status(statusCode).render("error.ejs", {message});
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went wrong!" } = err;
+  res.status(statusCode).render("error.ejs", { message });
 });
 
-app.listen(8080, () => {
-    console.log("listening on port 8080");
-})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`istening on port ${PORT}`);
+});
